987356b285a847ea3c72019bdcf6133b945d9852
Samuel Ballé <sballe@sbcmaroc.com>
2017-11-25 15:32:49 +0100

    no git copy as already existing with vagrant

--- setup/vagrant-setup.sh
+++ setup/vagrant-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ln -sf /vagrant /opt/debattons
-su - ubuntu -c "/opt/debattons/setup/dev-env-on-ubuntu-16.04.sh"
+su - ubuntu -c "/opt/debattons/setup/dev-env-on-ubuntu-16.04.sh --no-debattons-git-copy"
 su - ubuntu -c "/bin/bash /opt/debattons/scripts/build-and-run.dev.sh --start-orientdb-server"
 
