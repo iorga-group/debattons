b64ee449023b3fb562f8d95c84b0dd49b3e27ce3
Vianney M <vmaynadier@iorga.com>
2017-12-12 18:00:21 +0100

    Upgrading to Angular 5.0 + switching html editor to PrimeNG

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import {HttpModule} from '@angular/http';
 
@@ -7,13 +7,16 @@ import {AppComponent} from './app.component';
 import {ReactionService} from "./reaction.service";
 import {FormsModule} from "@angular/forms";
 
-import {LMarkdownEditorModule} from 'ngx-markdown-editor';
 import {ReactionEditorComponent} from "./reaction-editor.component";
 import {AppRoutingModule} from "./app-routing.module";
 import {ReactionDetailComponent} from "./reaction-detail.component";
 import {ReactionListComponent} from "./reaction-list.component";
 import {RootReactionsComponent} from "./root-reactions.component";
 import {NewReactionComponent} from "./new-reaction.component";
+
+import {EditorModule} from 'primeng/primeng';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -26,9 +29,10 @@ import {NewReactionComponent} from "./new-reaction.component";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    EditorModule,
     HttpModule,
     FormsModule,
-    LMarkdownEditorModule,
     AppRoutingModule
   ],
   providers: [
