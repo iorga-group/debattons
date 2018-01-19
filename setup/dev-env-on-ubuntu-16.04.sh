4a4a8385fd9e647ac73e71817228ef8932158842
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-01-20 10:23:35 +1100

    Install wget in dev-env-on-ubuntu-16.04.sh

--- setup/dev-env-on-ubuntu-16.04.sh
+++ setup/dev-env-on-ubuntu-16.04.sh
@@ -38,6 +38,9 @@ sudo -E apt-get update && sudo -E apt-get install yarn
 # Install angular-cli
 sudo -E yarn global add @angular/cli@1.5.3
 
+# Install wget
+sudo -E apt-get install -y wget
+
 # Install OrientDB tools (inspired by the Docker script https://github.com/orientechnologies/orientdb-docker/blob/e6855db21e0c4eb47db36f60a7b8a4d05ef5ffb4/3.0/x86_64/alpine/Dockerfile)
 ORIENTDB_VERSION=3.0.0RC1
 ORIENTDB_DOWNLOAD_MD5=f231735d7ac8c18e8e5f6aab5e6ae459
